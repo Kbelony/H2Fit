0acb6c1ccce2354cca10cb80034b9dac241c11f1
Kalvin <kalvin.sgb@outlook.com>
2023-09-11 23:03:54 +0200

    🚧 : add navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./assets/style.scss";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-white">H2 Fit!</h1>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold text-white">test!</h1>;
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="nabar-component">
+      <div className="desktop-part">
+        <img className="mt-4 ml-4" src="src/assets/img/Logo.svg" alt="" />
+      </div>
+      <div className="mobile-part"></div>
+    </div>
+  );
+};
+
+export default Navbar;
