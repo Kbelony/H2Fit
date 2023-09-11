56715ee90c3f0ec082ab7530afcc2a4e09c59cc0
Kalvin <kalvin.sgb@outlook.com>
2023-09-11 22:44:29 +0200

    🚧 : change text h1 App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./assets/style.scss";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold text-white">H2 Fit!</h1>;
 }
 
 export default App;
