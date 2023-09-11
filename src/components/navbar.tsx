b4505e92a8ba6a1d3002cdf5a0a7e5987a19737d
Kalvin <kalvin.sgb@outlook.com>
2023-09-11 23:06:20 +0200

    🐛 : fix path img logo.svg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,10 @@
+import Logo from "../assets/img/Logo.svg";
+
 const Navbar = () => {
   return (
     <div className="nabar-component">
       <div className="desktop-part">
-        <img className="mt-4 ml-4" src="src/assets/img/Logo.svg" alt="" />
+        <img className="mt-4 ml-4" src={Logo} alt="" />
       </div>
       <div className="mobile-part"></div>
     </div>
