b680afef07b1196706169cf4e2de288bea4a1bce
Kalvin <kalvin.sgb@outlook.com>
2023-09-13 21:45:29 +0200

    🚧 : add translate button

---
+++ src/components/InfoSteps.tsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
+
+const InfoSteps = () => {
+  const { language } = useContext(LanguageContext) || { language: "en" };
+
+  interface Translations {
+    [key: string]: {
+      starting: string;
+      age: string;
+    };
+  }
+
+  const translations: Translations = {
+    fr: {
+      starting:
+        "Pour commencer a vous créer un programme ils nous faudrait quelques informations",
+      age: "Quel age avez vous ?",
+    },
+    en: {
+      starting:
+        "To start creating a program for you we would need some information",
+      age: "How old are you?",
+    },
+  };
+
+  const translationKey = language || "en";
+  const { starting, age } = translations[translationKey];
+
+  return (
+    <div className="info-step-component">
+      <div className="flex flex-col items-center justify-center">
+        <h5 className="text-white text-base text-center p-7 pb-0">
+          {starting}
+        </h5>
+        <h5 className="text-white mt-3 text-3xl">{age}</h5>
+        <div className="container md:mt-25 mt-48">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="text-white text-center">1</div>
+            <div className="text-white text-center">1</div>
+            <div className="text-white text-center">1</div>
+            <div className="text-white text-center">1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoSteps;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,76 @@
+import { useContext, useState } from "react";
 import Logotext from "../assets/img/Logo-text.svg";
+import world from "../assets/img/world.svg";
+import { LanguageContext } from "./LanguageContext";
 
 const Navbar = () => {
+  const languageContext = useContext(LanguageContext);
+  const switchLanguage = languageContext?.switchLanguage || (() => {});
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
   return (
-    <div className="navbar-component mb-4">
-      <div className="desktop-part">
-        <img className="mt-9 ml-9 w-28" src={Logotext} alt="" />
+    <nav>
+      <div className="navbar-component mb-16">
+        <ul className="nav-list pt-5 pl-9 ">
+          <li className="nav-list-item md:flex md:justify-start">
+            <picture className="logo">
+              <img className="w-28 ml-0 md:ml-20 md:w24" src={Logotext} />
+            </picture>
+          </li>
+          <li className="nav-list-item md:flex md:justify-end">
+            <picture
+              className="world"
+              id="menu-button"
+              aria-expanded={isDropdownOpen}
+              aria-haspopup="true"
+              onClick={toggleDropdown}
+            >
+              <img className="w-7 mr-8 mt-2" src={world} />
+            </picture>
+            <div
+              className={`translation-dropdown absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                isDropdownOpen ? "" : "hidden"
+              }`}
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+            >
+              <div className="py-1" role="none">
+                <a
+                  className="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  id="menu-item-1"
+                  onClick={() => {
+                    switchLanguage("fr");
+                    closeDropdown();
+                  }}
+                >
+                  🇫🇷 Français
+                </a>
+                <a
+                  className="text-gray-700 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  id="menu-item-2"
+                  onClick={() => {
+                    switchLanguage("en");
+                    closeDropdown();
+                  }}
+                >
+                  🇺🇸 English
+                </a>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
-      <div className="mobile-part">
-        <img className="mt-6 ml-5 w-24" src={Logotext} alt="" />
-      </div>
-    </div>
+    </nav>
   );
 };
 
