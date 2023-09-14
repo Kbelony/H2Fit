fa8728bb9cc5a4c75dc9282ae7babfd8c9f7db51
Kalvin <kalvin.sgb@outlook.com>
2023-09-14 15:39:36 +0200

    🐛 : bug "Cannot find module './components/Navbar'"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./assets/style.scss";
 import InfoSteps from "./components/InfoSteps";
 import { LanguageProvider } from "./components/LanguageContext";
-import Navbar from "./components/Navbar";
+import Navbar from "../src/components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <LanguageProvider>
-        <Navbar />
+        <Navbar></Navbar>
         <InfoSteps />
       </LanguageProvider>
     </div>
