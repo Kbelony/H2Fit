4e9b9524e5fb0aeb718b3c129aeee55b7e0342f0
Kalvin <kalvin.sgb@outlook.com>
2023-09-16 03:39:56 +0200

    🚧 : add style desktop-view Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,6 +4,7 @@ import bar from "../assets/img/bar.svg";
 import apple from "../assets/img/apple.svg";
 import google from "../assets/img/Google.svg";
 import twitter from "../assets/img/Twitter.svg";
+import pexels from "../assets/img/pexels.jpeg";
 
 const Login = () => {
   const { language } = useContext(LanguageContext) || { language: "en" };
@@ -112,7 +113,69 @@ const Login = () => {
           </div>
         </div>
       </div>
-      <div className="desktop-view"></div>
+      <div className="desktop-view">
+        <div className="flex flex-grow justify-between">
+          <div className="flex ml-60 mt-16 flex-col items-start justify-start">
+            <div className="container md:mt-12 mt-8">
+              <div className="flex flex-col justify-start mr-10">
+                <div className="login-form ml-8">
+                  <div className="text">
+                    <h5 className="text-white text-xl text-center p-7 pb-0">
+                      {welcome}
+                    </h5>
+                    <h5 className="text-white details text-center text-base">
+                      {details}
+                    </h5>
+                  </div>
+                  <div className="group-social mt-10 flex flex-row justify-center">
+                    <div className="apple-btn social-btn mr-4 py-3 px-10">
+                      <img src={apple} alt="" />
+                    </div>
+                    <div className="google-btn social-btn mr-4 py-3 px-10">
+                      <img src={google} alt="" />
+                    </div>
+                    <div className="twitter-btn social-btn py-3 px-10">
+                      <img src={twitter} alt="" />
+                    </div>
+                  </div>
+                  <img className="bar mt-5 mb-10 w-full" src={bar} alt="" />
+                  <form onSubmit={handleSubmit}>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="mb-6 flex flex-col"
+                      placeholder={email}
+                    />
+
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="mb-6 flex flex-col"
+                      placeholder={password}
+                    />
+
+                    <button
+                      className="text-white py-3 px-6 submit-btn"
+                      type="submit"
+                    >
+                      {submit}
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex mt-4 right-img flex-col">
+            <img src={pexels} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
