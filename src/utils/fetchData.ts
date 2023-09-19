fd1f1b4769ed795cc3e795d6254337a3b75dda5d
Kalvin <kalvin.sgb@outlook.com>
2023-09-19 17:38:38 +0200

    🐛 : fix error api quota

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -3,7 +3,7 @@ export const options = {
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': '24f2c66b83msh91fbdb3c8a6b710p173768jsn1bd187c8731c',
+      'X-RapidAPI-Key': '483bec1f24msh8457a564c70f4cep1e6128jsn2433b5d9a1aa',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
