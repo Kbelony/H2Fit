5a926717dc396aa97f1a9b8023f392a5ed44ee70
Kalvin <kalvin.sgb@outlook.com>
2023-09-20 15:10:02 +0200

    🚧 : add firebase articles (no style)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { RootState } from "../app/store";
 import { useSelector } from "react-redux";
 import { options, fetchData } from "../utils/fetchData";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -13,7 +15,49 @@ const Home = () => {
   const { language } = useContext(LanguageContext) || { language: "en" };
   const user = useSelector((state: RootState) => state.user);
   const userName = user.name;
-  const [exercicesData, setExercicesData] = useState([]); // État pour stocker les données
+  const [exercicesData, setExercicesData] = useState([]);
+  const [postLists, setPostList] = useState<
+    {
+      title: string;
+      author: string;
+      img_1: string;
+      img_author: string;
+      paragraph_1: string;
+      paragraph_2: string;
+      paragraph_3: string;
+      type: string;
+    }[]
+  >([]);
+
+  const postCollectionRef = collection(db, "articles");
+
+  const getPosts = async () => {
+    const data = await getDocs(postCollectionRef);
+    setPostList(
+      data.docs.map((doc) => ({
+        ...doc.data(),
+        title: doc.data().title,
+        author: doc.data().author,
+        img_1: doc.data().img_1,
+        img_author: doc.data().img_author,
+        paragraph_1: doc.data().paragraph_1,
+        paragraph_2: doc.data().paragraph_2,
+        paragraph_3: doc.data().paragraph_3,
+        type: doc.data().type,
+      }))
+    );
+  };
+
+  // const getTease = async () => {
+  //   const data = await getDocs(teaseCollectionRef);
+  //   setTeaseList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  // };
+
+  useEffect(() => {
+    getPosts();
+    console.log(postLists);
+    // getTease();
+  }, []);
 
   async function fetchExercicesData() {
     try {
@@ -59,11 +103,19 @@ const Home = () => {
   const translationKey = language || "en";
   const { hi, foryou, bodylist } = translations[translationKey];
 
-  const settings = {
+  const settingsBody = {
     slidesPerView: 3,
     centeredSlides: false,
     spaceBetween: 1,
-    loop: false,
+    loop: true,
+    navigation: true,
+  };
+
+  const settingsArticles = {
+    slidesPerView: 2,
+    centeredSlides: false,
+    spaceBetween: 1,
+    loop: true,
     navigation: true,
   };
 
@@ -78,11 +130,11 @@ const Home = () => {
           {foryou}
         </div>
         <div className="flex flex-col items-center justify-center">
-          <div className="container md:mt-12 mt-6">
+          <div className="container mt-6">
             <div className="text-2xl mb-3 ml-4 text-white">{bodylist}</div>
             <div className="flex bodypart-container">
               <Swiper
-                {...settings}
+                {...settingsBody}
                 modules={[Navigation]}
                 className="swiper-container"
               >
@@ -96,6 +148,31 @@ const Home = () => {
                           alt={bodyPart}
                         />
                         <p className="text-white text-base">{bodyPart}</p>
+                      </div>
+                    </Link>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+          <div className="container mt-1">
+            <div className="text-2xl mb-3 ml-4 text-white">{bodylist}</div>
+            <div className="flex bodypart-container">
+              <Swiper
+                {...settingsArticles}
+                modules={[Navigation]}
+                className="swiper-container"
+              >
+                {postLists.map((articles, index) => (
+                  <SwiperSlide key={index}>
+                    <Link to={`/home/${articles}`}>
+                      <div className="mb-5" key={index}>
+                        <img
+                          src={articles.img_1}
+                          // Assurez-vous d'avoir les images correspondantes dans votre répertoire public
+                          alt={articles.title}
+                        />
+                        <p className="text-white text-base">{articles.title}</p>
                       </div>
                     </Link>
                   </SwiperSlide>
