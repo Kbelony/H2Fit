df444d0006d15a6f31cbe0d1b0000d16bed55031
Kalvin <kalvin.sgb@outlook.com>
2023-10-03 16:50:16 +0200

    🚧 : add bottom-bar

---
+++ src/components/BottomBar.tsx
@@ -0,0 +1,16 @@
+const BottomBar = () => {
+  return (
+    <div className="bottom-bar-component">
+      <div className="mobile-view">
+        <div className="bar px-10 py-4 w-full flex justify-between">
+          <div className="active text-white">breif</div>
+          <div className="text-white">aeaz</div>
+          <div className="">aeaz</div>
+        </div>
+      </div>
+      <div className="desktop-view">Bottom</div>
+    </div>
+  );
+};
+
+export default BottomBar;
